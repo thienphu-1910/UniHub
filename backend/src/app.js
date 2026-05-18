3fb5b7940a9b98249484b117fa35674561d0b3c3
Truong Cong Thien Phu <truongcongthienphu1910.work@gmail.com>
2026-05-18 12:09:40 +0700

    feat: allow vercel origin in cors options

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ import redis from "./config/redis.js";
 
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://uni-hub-delta.vercel.app"],
 
   credentials: true,
 
